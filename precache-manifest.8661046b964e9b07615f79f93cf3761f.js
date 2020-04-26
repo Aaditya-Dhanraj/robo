71ecc2d43ba4dfc23cc308f0a46db5eab1fcf834
Aaditya Dhanraj <aadityaiiitu@gmail.com>
2020-04-27 02:57:12 +0530

    Updates

---
+++ precache-manifest.8661046b964e9b07615f79f93cf3761f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826acb6b281b51f280deafb33e904f3d",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "d8b099112545a09d2291",
+    "url": "/robo/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "9fa3636de5a6737c9c31",
+    "url": "/robo/static/css/main.f55723e1.chunk.css"
+  },
+  {
+    "revision": "d8b099112545a09d2291",
+    "url": "/robo/static/js/2.4cf2d9f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.4cf2d9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa3636de5a6737c9c31",
+    "url": "/robo/static/js/main.2d5c07de.chunk.js"
+  },
+  {
+    "revision": "027a0ac7a9a9ea05fa83",
+    "url": "/robo/static/js/runtime-main.dbdd0969.js"
+  },
+  {
+    "revision": "c40de6415faa48a861478f0ae43e21c7",
+    "url": "/robo/static/media/MinecraftTen-VGORe.c40de641.woff"
+  }
+]);
